7db7d7fb9ed5c72e29e311e9bf2b7e5e77d2cf5e
ayulintanggayanti <ayu.119140095@student.itera.ac.id>
2022-05-30 13:25:24 +0700

    create component footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div class="footer mt-16 bg-gray-700 py-4 text-gray-50">
+            <div class="text-center">
+                <span>2022 &copy; Made with love by Ayu Lintang</span>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
