2da9a95085cf1c7b92bbacbf6d22f5d7dca4e4c1
ayulintanggayanti <ayu.119140095@student.itera.ac.id>
2022-05-30 13:18:29 +0700

    add tailwind css config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./public/index.html",
+    ],
+    theme: {
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            // xxl: "1440px",
+        },
+    },
+    plugins: [],
+};
