8148cd29b2f066a6adf52f53964044923b048e5e
ayulintanggayanti <ayu.119140095@student.itera.ac.id>
2022-05-30 13:25:13 +0700

    create component message

---
+++ src/components/Message.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Message = () => {
+    return (
+        <div className="mt-10 px-5">
+            <div classname="container justify-center flex">
+                <div class="w-full pt-5 text-gray-100">
+                    <h1 class="bg-gray-700 px-5 py-5 rounded-xl">Test Komentar 1</h1>
+                </div>
+                <div class="w-full pt-5 text-gray-100">
+                    <h1 class="bg-gray-700 px-5 py-5 rounded-xl">Test Komentar 2</h1>
+                </div>
+                <div class="w-full pt-5 text-gray-100">
+                    <h1 class="bg-gray-700 px-5 py-5 rounded-xl">Test Komentar 3</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Message;
